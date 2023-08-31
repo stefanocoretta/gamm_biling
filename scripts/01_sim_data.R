266c38f26d0d21acca71940fb682026e19456a09
Joseph V. Casillas <joseph.casillas@rutgers.edu>
2023-08-31 13:26:59 -0400

    add u-shaped development simple simulation

--- scripts/01_sim_data.R
+++ scripts/01_sim_data.R
@@ -18,3 +18,29 @@ ysim <- function(n = 500, scale = 2) {
 df <- ysim()
 
 plot(df$x2, df$y)
+
+
+# JVC attempt -----------------------------------------------------------------
+
+# Cross-sectional design (Based on Lightbrown's study of French Canadian
+# learners of English)
+# - test 100 learners on -ing production based on proficiency
+# - low proficiency score higher because they generalize -ing to all forms
+# - med proficiency score lower because they begin using -ing and present for
+#   progressive
+# - high proficiency score highest because they learn to only use -ing for
+#   progressive
+library("dplyr")
+
+n <- 100
+
+dat <- tibble(
+  beta_0 = 15, # slope controls range of scores
+  proficiency = runif(n, -2, 2), # suppose standardized prof. score
+  score = beta_0 + proficiency^2 + rnorm(n, 0, .5) # gen. parabola
+) %>%
+  mutate(score = score / max(score) * 100) %>% # scale to 0-100
+  # no perfect scores at low proficiency (generalization stragegy isn't 100% accurate)
+  mutate(score = if_else(proficiency < -1, .$score - 10, .$score - 5))
+
+plot(x = dat$proficiency, y = dat$score)
